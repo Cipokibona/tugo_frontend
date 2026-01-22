e3617b5a7746a97ad445b1872757cfdd426c5dab
Cipo <cipokibona.ck@gmail.com>
2026-01-22 13:52:41 +0200

    login

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ServiceApi } from '../../services/service-api';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  loginForm: FormGroup;
+  loading = false;
+  errorMessage: string | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private service: ServiceApi,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  submit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    this.loading = true;
+    this.errorMessage = null;
+
+    this.service.login(username, password).subscribe({
+      next: () => {
+        this.loading = false;
+        this.router.navigate(['/home']).then(() => {
+          window.location.reload();
+          console.log('Navigation to /home successful');
+        });
+      },
+      error: (err) => {
+        this.loading = false;
+        this.errorMessage =
+          err?.error?.detail || 'Username or password incorrect';
+      }
+    });
+
+  }
 }
--- src/app/services/service-api.ts
+++ src/app/services/service-api.ts
@@ -1,8 +1,42 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject,Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+interface TokenResponse {
+  access: string;
+  refresh: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServiceApi {
-  
+
+  private http = inject(HttpClient);
+  private url = 'http://127.0.0.1:8000/api/';
+
+  private token: string | null = null;
+
+  private tokenUrl = `${this.url}token/`;
+  private refreshUrl = `${this.url}token/refresh/`;
+  private registerUrl = `${this.url}register/`;
+
+   constructor() { }
+
+  login(username: string, password: string) {
+    return this.http.post<{ access: string; refresh: string }>(this.tokenUrl, { username, password })
+      .pipe(tap(response => {
+        this.token = response.access;
+        localStorage.setItem('token', this.token);
+        localStorage.setItem('refresh_token', response.refresh);
+      }));
+  }
+
+  logout() {
+    this.token = null;
+    localStorage.removeItem('token');
+    localStorage.removeItem('refresh_token');
+    window.location.href = '/login';
+  }
+
 }
+
